cc2f763b109e4a680ee03dc9d0ee6cef06655ca7
ReactiveMatter <reactivematter@protonmail.com>
2026-06-14 23:28:21 +0530

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -5,6 +5,7 @@
  */
 
 $conf['fullWidthSite'] = 1;
-$meta['inlineToc']= 0;
+$conf['inlineToc']= 0;
 $conf['showPageInfo'] = 1;
 $conf['theme'] = 'Default';
+$conf['bottomToolBar'] = 'page,site,user';
